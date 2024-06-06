3fd634e57da3641768e8a9e9a04b44287bc524ff
Ian K Armstrong <ian.k.armstrong@gmail.com>
2024-06-06 11:16:59 +0200

    update

---
+++ lib/dort/page.dart
@@ -0,0 +1,103 @@
+import 'package:confesion_de_fe_de_westminster/about/model.dart';
+import 'package:confesion_de_fe_de_westminster/about/queries.dart';
+import 'package:confesion_de_fe_de_westminster/bloc/bloc_font.dart';
+import 'package:confesion_de_fe_de_westminster/bloc/bloc_italic.dart';
+import 'package:confesion_de_fe_de_westminster/bloc/bloc_size.dart';
+import 'package:confesion_de_fe_de_westminster/fonts/list.dart';
+import 'package:confesion_de_fe_de_westminster/utils/globals.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+// About
+
+AbQueries abQueries = AbQueries();
+
+class AboutPage extends StatefulWidget {
+  const AboutPage({super.key});
+
+  @override
+  AboutPageState createState() => AboutPageState();
+}
+
+class AboutPageState extends State<AboutPage> {
+  List<About> paragraphs = List<About>.empty();
+  String heading = "About";
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   primaryTextSize = BlocProvider.of<TextSizeCubit>(context).state;
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    // final args =
+    //     ModalRoute.of(context)!.settings.arguments as PrefPageArguments;
+
+    return FutureBuilder<List<About>>(
+      future: abQueries.getParagraphs(),
+      builder: (context, AsyncSnapshot<List<About>> snapshot) {
+        if (snapshot.hasData) {
+          paragraphs = snapshot.data!;
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              elevation: 5,
+              leading: GestureDetector(
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Future.delayed(
+                      Duration(milliseconds: Globals.navigatorDelay),
+                      () {
+                        Navigator.pop(context);
+                      },
+                    );
+                  },
+                ),
+              ),
+              title: Text(heading,
+                  style: const TextStyle(fontWeight: FontWeight.w700)
+                  // style: const TextStyle(
+                  //   color: Colors.yellow,
+                  // ),
+                  ),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.builder(
+                itemCount: paragraphs.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(
+                      paragraphs[index].h!,
+                      style: TextStyle(
+                          fontFamily: fontsList[context.read<FontBloc>().state],
+                          fontWeight: FontWeight.w700,
+                          fontStyle: (context.read<ItalicBloc>().state)
+                              ? FontStyle.italic
+                              : FontStyle.normal,
+                          fontSize: context.read<SizeBloc>().state),
+                    ),
+                    subtitle: Text(
+                      paragraphs[index].t!,
+                      style: TextStyle(
+                          fontFamily: fontsList[context.read<FontBloc>().state],
+                          fontStyle: (context.read<ItalicBloc>().state)
+                              ? FontStyle.italic
+                              : FontStyle.normal,
+                          fontSize: context.read<SizeBloc>().state),
+                    ),
+                  );
+                },
+              ),
+            ),
+          );
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
